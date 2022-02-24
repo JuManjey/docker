7ccf1c9ba8bb43aea95a031fc290e778ca02c713
JuManji <opghld@gmail.com>
2022-02-24 18:58:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# Install docker
 #!/bin/bash
 sudo apt update
 sudo apt install wget -y
@@ -6,3 +7,16 @@ sudo sh get-docker.sh
 sudo apt install -y docker-compose
 rm -f get-docker.sh
 sudo usermod -aG docker $USER
+
+# Isntall docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+# Upgrading🔗
+docker-compose migrate-to-labels
+
+
+
+
+
