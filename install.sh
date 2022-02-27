b96cf5b2ffcf23ec7e02c01c4735a99b308bccc4
JuManji <opghld@gmail.com>
2022-02-27 16:04:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # Install docker
 #!/bin/bash
-sudo apt update
-sudo apt install wget -y
-wget -O get-docker.sh https://get.docker.com
-sudo sh get-docker.sh
-sudo apt install -y docker-compose
-rm -f get-docker.sh
+sudo apt update &&
+sudo apt install wget -y &&
+wget -O get-docker.sh https://get.docker.com &&
+sudo sh get-docker.sh &&
+sudo apt install -y docker-compose &&
+rm -f get-docker.sh &&
 sudo usermod -aG docker $USER
 
 # Isntall docker-compose
